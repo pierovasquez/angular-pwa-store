48a39c7eeb4ec2735ca508f796becd13be31fa52
Piero Vasquez <piero.vasquez.r@gmail.com>
2020-12-09 22:02:41 +0100

    dumb components modified so they work with the smart components now

--- src/app/admin/products/components/product-create/product-create.component.ts
+++ src/app/admin/products/components/product-create/product-create.component.ts
@@ -1,29 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
 
-import { finalize } from 'rxjs/operators';
+import { finalize, takeUntil } from 'rxjs/operators';
 
 import { MyValidators } from './../../../../utils/validators';
-import { ProductsService } from './../../../../core/services/products/products.service';
 
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { Product } from 'src/app/core/models/product.model';
+import { Category } from 'src/app/core/models/category.model';
 
 @Component({
   selector: 'app-product-create',
   templateUrl: './product-create.component.html',
   styleUrls: ['./product-create.component.scss']
 })
-export class ProductCreateComponent implements OnInit {
+export class ProductCreateComponent implements OnInit, OnDestroy {
 
+  @Input()
+  set categories(data: Category[]) {
+    if (data) {
+      this._categories = data;
+    }
+  }
+  get categories(): Category[] {
+    return this._categories;
+  }
+  @Output() save: EventEmitter<Product> = new EventEmitter<Product>();
+
+  private progress: number;
+  private _categories: Category[];
   form: FormGroup;
+  showProgressBar: boolean;
   image$: Observable<any>;
 
+  private $destroy = new Subject<void>();
   constructor(
     private formBuilder: FormBuilder,
-    private productsService: ProductsService,
-    private router: Router,
     private storage: AngularFireStorage
   ) {
     this.buildForm();
@@ -32,35 +45,57 @@ export class ProductCreateComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.$destroy.next();
+    this.$destroy.complete();
+  }
+
   saveProduct(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
       const product = this.form.value;
-      this.productsService.createProduct(product)
-      .subscribe((newProduct) => {
-        console.log(newProduct);
-        this.router.navigate(['./admin/products']);
-      });
+      this.save.emit(product);
+    } else {
+      this.form.markAllAsTouched();
     }
   }
 
   uploadFile(event) {
-    const file = event.target.files[0];
-    const name = 'image.png';
-    const fileRef = this.storage.ref(name);
-    const task = this.storage.upload(name, file);
+    if (event.target.files && event.target.files.length > 0) {
+      this.showProgressBar = true;
+      const file = event.target.files[0];
+      const name = 'image.png';
+      const fileRef = this.storage.ref(name);
+      const task = this.storage.upload(name, file);
+      this.subscribeToPercentChanges(task);
+      this.subscribeToSnapshotChanges(task, fileRef);
 
+    }
+  }
+
+  private subscribeToPercentChanges(task: AngularFireUploadTask) {
+    task.percentageChanges().pipe(takeUntil(this.$destroy)).subscribe(value => {
+      this.progress = value;
+      if (value === 100) {
+        setTimeout(() => {
+          this.showProgressBar = false;
+        }, 400);
+      }
+    });
+  }
+
+  private subscribeToSnapshotChanges(task: AngularFireUploadTask, fileRef: AngularFireStorageReference) {
     task.snapshotChanges()
-    .pipe(
-      finalize(() => {
-        this.image$ = fileRef.getDownloadURL();
-        this.image$.subscribe(url => {
-          console.log(url);
-          this.form.get('image').setValue(url);
-        });
-      })
-    )
-    .subscribe();
+      .pipe(
+        finalize(() => {
+          this.image$ = fileRef.getDownloadURL();
+          this.image$.subscribe(url => {
+            console.log(url);
+            this.form.get('image').setValue(url);
+          });
+        })
+      )
+      .subscribe();
   }
 
   private buildForm() {
@@ -73,6 +108,10 @@ export class ProductCreateComponent implements OnInit {
     });
   }
 
+  get imageField() {
+    return this.form.get('image');
+  }
+
   get nameField() {
     return this.form.get('name');
   }
@@ -83,6 +122,14 @@ export class ProductCreateComponent implements OnInit {
 
   get priceField() {
     return this.form.get('price');
+  }
+
+  get categoryField() {
+    return this.form.get('category_id');
+  }
+
+  get progressValue() {
+    return this.progress;
   }
 
 }
--- src/app/admin/products/components/product-edit/product-edit.component.ts
+++ src/app/admin/products/components/product-edit/product-edit.component.ts
@@ -1,63 +1,135 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { MyValidators } from './../../../../utils/validators';
-import { ProductsService } from './../../../../core/services/products/products.service';
+import { Product } from 'src/app/core/models/product.model';
+import { Category } from 'src/app/core/models/category.model';
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask } from '@angular/fire/storage';
+import { finalize, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
   styleUrls: ['./product-edit.component.scss']
 })
-export class ProductEditComponent implements OnInit {
+export class ProductEditComponent implements OnInit, OnDestroy {
+
+  @Input()
+  set product(data: Product) {
+    if (data) {
+      this.form.patchValue(data);
+    }
+  }
+
+  @Input()
+  set categories(data: Category[]) {
+    if (data) {
+      this._categories = data;
+    }
+  }
+  get categories(): Category[] {
+    return this._categories;
+  }
+
+  @Output() update: EventEmitter<Product> = new EventEmitter<Product>();
 
   form: FormGroup;
-  id: string;
+  showProgressBar: boolean;
 
+  private progress: number;
+  private _categories: Category[];
+  private $destroy = new Subject<void>();
   constructor(
     private formBuilder: FormBuilder,
-    private productsService: ProductsService,
-    private router: Router,
-    private activatedRoute: ActivatedRoute
+    private storage: AngularFireStorage
   ) {
     this.buildForm();
   }
 
   ngOnInit() {
-    this.activatedRoute.params.subscribe((params: Params) => {
-      this.id = params.id;
-      this.productsService.getProduct(this.id)
-      .subscribe(product => {
-        this.form.patchValue(product);
-      });
+  }
+
+  ngOnDestroy() {
+    this.$destroy.next();
+    this.$destroy.complete();
+  }
+
+  uploadFile(event) {
+    if (event.target.files && event.target.files.length > 0) {
+      this.showProgressBar = true;
+      const file = event.target.files[0];
+      const name = 'image.png';
+      const fileRef = this.storage.ref(name);
+      const task = this.storage.upload(name, file);
+      this.subscribeToPercentChanges(task);
+      this.subscribeToSnapshotChanges(task, fileRef);
+
+    }
+  }
+
+  private subscribeToPercentChanges(task: AngularFireUploadTask) {
+    task.percentageChanges().pipe(takeUntil(this.$destroy)).subscribe(value => {
+      this.progress = value;
+      if (value === 100) {
+        setTimeout(() => {
+          this.showProgressBar = false;
+        }, 400);
+      }
     });
+  }
+
+  private subscribeToSnapshotChanges(task: AngularFireUploadTask, fileRef: AngularFireStorageReference) {
+    task.snapshotChanges()
+      .pipe(
+        finalize(() => {
+          fileRef.getDownloadURL().subscribe(url => {
+            this.form.get('image').setValue(url);
+          });
+        })
+      )
+      .subscribe();
   }
 
   saveProduct(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
-      const product = this.form.value;
-      this.productsService.updateProduct(this.id, product)
-      .subscribe((newProduct) => {
-        console.log(newProduct);
-        this.router.navigate(['./admin/products']);
-      });
+      this.update.emit(this.form.value);
+    } else {
+      this.form.markAllAsTouched();
     }
   }
 
   private buildForm() {
     this.form = this.formBuilder.group({
-      id: ['', [Validators.required]],
-      title: ['', [Validators.required]],
+      _id: ['', [Validators.required]],
+      category_id: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(4)]],
       price: ['', [Validators.required, MyValidators.isPriceValid]],
-      image: [''],
-      description: ['', [Validators.required]],
+      image: ['', [Validators.required]],
+      description: ['', [Validators.required, Validators.minLength(10)]],
     });
   }
 
+  get nameField() {
+    return this.form.get('name');
+  }
+  get categoryField(){
+    return this.form.get('category_id');
+  }
   get priceField() {
     return this.form.get('price');
+  }
+
+  get imageField() {
+    return this.form.get('image');
+  }
+
+  get descriptionField() {
+    return this.form.get('description');
+  }
+  get progressValue() {
+    return this.progress;
   }
 
 }
